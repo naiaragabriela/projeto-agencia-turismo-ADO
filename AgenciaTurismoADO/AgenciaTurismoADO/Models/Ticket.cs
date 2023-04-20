98f9f50ca81b92da3c18e108a0aaad013e4f9cd6
naiaragabriela <naigabi31@gmail.com>
2023-04-20 20:49:15 -0300

    criacao das models

---
+++ AgenciaTurismoADO/AgenciaTurismoADO/Models/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaTurismoADO.Models
+{
+    public class Ticket
+    {
+        #region Properties
+        public int Id { get; set; }
+        public Address Origin { get; set; }
+        public Address Destination { get; set; }
+        public Client Client { get; set; }
+        public DateTime DtRegistration { get; set; }
+        public decimal CostTicket { get; set; }
+        #endregion
+
+        public override string ToString()
+        {
+            return "Custo: " + CostTicket +
+                   "\nOrigem da Passagem: " + Origin.ToString() +
+                   "\nDestino da Passagem: " + Destination.ToString() +
+                   "\nCliente da Passagem: " + Client.ToString();
+        }
+    }
+}
